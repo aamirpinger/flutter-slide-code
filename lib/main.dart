97072d23597168b68303078597cf2b114a850f85
Aamir Pinger <aamirpinger@yahoo.com>
2021-11-04 19:27:42 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,3 @@ void main() {
     ),
   );
 }
-
-class myApp extends StatelessWidget {
-  const myApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
