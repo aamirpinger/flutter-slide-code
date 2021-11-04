b73f34d515d449358932d0fd2f5825c84697a666
Aamir Pinger <aamirpinger@yahoo.com>
2021-11-04 20:37:59 +0500

    example: column widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,24 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepOrange,
         ),
         body: SafeArea(
-          child: Container(
-            child: Text(
-              'Hello World',
-              style: TextStyle(fontSize: 24, color: Colors.white),
-            ),
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
-            margin: EdgeInsets.only(left: 10, top: 20),
-            color: Colors.deepOrange,
-            width: 150,
-            height: 70,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // verticalDirection: VerticalDirection.up, // try this
+            // mainAxisSize: MainAxisSize.min, // try this
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                child: Text(
+                  'Idea!!!',
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
+                color: Colors.lightBlue,
+              ),
+              Image(
+                image: AssetImage('images/bulb.jpg'),
+              ),
+            ],
           ),
         ),
       ),
