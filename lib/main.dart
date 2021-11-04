66249e7b938826bc4e3fec88d4674c2889e71b68
Aamir Pinger <aamirpinger@yahoo.com>
2021-11-04 19:25:54 +0500

    feature: app icons

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,12 @@ void main() {
     ),
   );
 }
+
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
