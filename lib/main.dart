16ca5d6fda679d3a38a997ea3d975d8cdef7eacb
Aamir Pinger <aamirpinger@yahoo.com>
2021-11-05 16:48:30 +0500

    Exercise rows & columns

--- lib/main.dart
+++ lib/main.dart
@@ -11,32 +11,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('My Flutter App'),
-          backgroundColor: Colors.deepOrange,
-        ),
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // verticalDirection: VerticalDirection.up, // try this
-            // mainAxisSize: MainAxisSize.min, // try this
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                child: Text(
-                  'Idea!!!',
-                  style: TextStyle(fontSize: 24, color: Colors.white),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
-                color: Colors.lightBlue,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.amberAccent,
+                    width: 195,
+                    height: 200,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 195,
+                    height: 200,
+                  ),
+                ],
               ),
-              CircleAvatar(
-                backgroundColor: Colors.brown.shade800,
-                // child: Text('AP'), // try this as well
-                backgroundImage: AssetImage(
-                    'images/bulb.jpg'), // you can try NetworkImage as well
-                radius: 100,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        image: DecorationImage(
+                          image: AssetImage('images/bulb.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 8,
+                        ),
+                        borderRadius: BorderRadius.circular(150),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white.withOpacity(0.5),
+                            spreadRadius: 25,
+                            blurRadius: 15,
+                          ),
+                        ]),
+                    width: 250,
+                    height: 250,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    color: Colors.purple,
+                    width: 195,
+                    height: 200,
+                  ),
+                  Container(
+                    color: Colors.lightGreenAccent,
+                    width: 195,
+                    height: 200,
+                  ),
+                ],
               ),
             ],
           ),
