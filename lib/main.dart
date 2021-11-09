9936ed923c13957e947f99b817f3a948faac46cf
Aamir Pinger <aamirpinger@yahoo.com>
2021-11-09 12:01:10 +0500

    example: Stateful widget - bulb

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text(
-        'Hello world',
-      ),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyBulb();
+  }
+}
+
+class MyBulb extends StatefulWidget {
+  const MyBulb({Key? key}) : super(key: key);
+
+  @override
+  _MyBulbState createState() => _MyBulbState();
+}
+
+class _MyBulbState extends State<MyBulb> {
+  bool isOff = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Bulb App'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  isOff ? Icons.lightbulb_outline : Icons.lightbulb,
+                  size: 200,
+                  color: isOff ? null : Colors.limeAccent,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(
+                      () {
+                        isOff = !isOff;
+                      },
+                    );
+                  },
+                  child: Text(
+                    'Turn ${isOff ? 'On' : 'Off'}',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
 }
