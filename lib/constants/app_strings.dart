0731f429f74ccf88e37e14397f1482e05a49103f
Aamir Pinger <aamirpinger@yahoo.com>
2021-11-25 11:00:45 +0500

    example dictionary app Loader

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -6,4 +6,5 @@ abstract class AppStrings {
   static const meaning = 'Meaning';
   static const playAudio = 'Play Audio';
   static const play = 'Play';
+  static const loading = 'loading....';
 }
--- lib/ui/screens/dictionary_app.dart
+++ lib/ui/screens/dictionary_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/ui/routes.dart';
 import 'package:my_app/ui/theme.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class DictionaryApp extends StatelessWidget {
   const DictionaryApp({Key? key}) : super(key: key);
@@ -13,6 +14,7 @@ class DictionaryApp extends StatelessWidget {
       theme: CustomTheme.theme,
       initialRoute: DictionaryApp.routeName,
       onGenerateRoute: CustomRoutes.generateRoute,
+      builder: EasyLoading.init(),
     );
   }
 }
