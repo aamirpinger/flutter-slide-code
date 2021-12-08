162dfd673b155e6806b6cca3dc8548570552a9b3
Aamir Pinger <aamirpinger@yahoo.com>
2021-12-08 10:01:40 +0500

    Example: Basic List State Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text(
-        'Hello world',
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Parent(),
+        ),
       ),
     ),
-  ));
+  );
+}
+
+class Parent extends StatelessWidget {
+  String value = "Hello from Parent Widget";
+
+  @override
+  Widget build(BuildContext context) {
+    return FirstChild(value: value);
+  }
+}
+
+class FirstChild extends StatelessWidget {
+  const FirstChild({required this.value});
+
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return GrandChild(value: value);
+  }
+}
+
+class GrandChild extends StatelessWidget {
+  const GrandChild({required this.value});
+
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      value,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 48,
+        color: Colors.red,
+      ),
+    );
+  }
 }
